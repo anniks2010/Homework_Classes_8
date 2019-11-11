56056198dc01a10255e8533d37b64739bd5bdba4
Anu <anniks_4@hotmail.com>
2019-11-11 22:16:08 +0200

    hamster

---
+++ Homework_classes_8/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework_classes_8
+{
+    internal class Animal
+    {
+        public string Name;
+        public int Age = 0;
+        public double Weight;
+        public int LIfeSpan = 0;
+
+        public void ToPrintAnimalBaseInfo()
+        {
+            Console.WriteLine($"Name is {Name}");
+            Console.WriteLine($"Age is {Age}");
+            Console.WriteLine($"Weight is {Weight}");
+            Console.WriteLine($"Lifespan is {LIfeSpan}");
+
+        }
+        public void ToEat()
+        {
+            Console.WriteLine("Eating");
+            Weight += 0.2;
+        }
+        public void ToDoExcerices()
+        {
+            Console.WriteLine("Running");
+            Weight -= 0.1;
+        }
+        
+    }
+}
--- Homework_classes_8/Program.cs
+++ Homework_classes_8/Program.cs
@@ -6,33 +6,6 @@ using System.Threading.Tasks;
 
 namespace Homework_classes_8
 {
-    class Animal
-    {
-        public string Name;
-        public int Age = 0;
-        public double Weight;
-        public int LIfeSpan = 0;
-
-        public void ToPrintAnimalBaseInfo()
-        {
-            Console.WriteLine($"Name is {Name}");
-            Console.WriteLine($"Age is {Age}");
-            Console.WriteLine($"Weight is {Weight}");
-            Console.WriteLine($"Lifespan is {LIfeSpan}");
-
-        }
-        public void ToEat()
-        {
-            Console.WriteLine("Eating");
-            Weight += 0.2;
-        }
-        public void ToDoExcerices()
-        {
-            Console.WriteLine("Running");
-            Weight -= 0.1;
-        }
-        
-    }
     class Program
     {
         static void Main(string[] args)
